b2bf4bf9b2e940658a464dbec9dfb50f2b4f98ca
Iaron da Costa Araújo <iaron.araujo@ccc.ufcg.edu.br>
2022-04-05 16:13:42 -0300

    Update store.config with the right account

--- store.config.js
+++ store.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   // Platform specific configs for API
   api: {
-    storeId: 'storeframework',
+    storeId: 'toys',
     environment: 'vtexcommercestable',
     hideUnavailableItems: true,
   },
