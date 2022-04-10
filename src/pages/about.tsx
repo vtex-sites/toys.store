c579df6d62343b4784dbc85b6b4681f8fc990284
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2022-04-10 19:17:47 -0300

    feat: about - page creation

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GatsbySeo } from 'gatsby-plugin-next-seo'
+
+function Page() {
+  return (
+    <>
+      <GatsbySeo noindex nofollow />
+      <div>TO DO</div>
+    </>
+  )
+}
+
+export default Page
