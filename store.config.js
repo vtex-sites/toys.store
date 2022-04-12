1a88f1fcfd42586f3676285dc02c8572be3355ca
Julia Amadio <71294182+JKAmadio@users.noreply.github.com>
2022-04-12 12:16:34 -0300

    fix: retorno storeId

--- store.config.js
+++ store.config.js
@@ -4,7 +4,7 @@ module.exports = {
 
   // Platform specific configs for API
   api: {
-    storeId: 'frncubo', // deixar frn por enquanto... original: toys
+    storeId: 'toys',
     environment: 'vtexcommercestable',
     hideUnavailableItems: true,
   },
